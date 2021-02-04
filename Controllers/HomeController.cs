3783da41ad2d58edef6a41fe0cd3f529bb9991cc
Samantha Berg <sb11158@byu.edu>
2021-02-03 21:11:10 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Assign3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assign3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Application()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Application(ApplicationResponse appResponse)
+        {
+            TempStorage.AddApplication(appResponse);
+            return View("Confirmation", appResponse);
+        }
+        public IActionResult Waitlist()
+        {
+            return View(TempStorage.Applications);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Podcasts()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
